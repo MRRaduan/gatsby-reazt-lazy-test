ca2ebd0e7c7ca9a49f6ed80dea7ba1b1f4e5a5d7
MRRaduan <administrador@MacBook-Pro-de-Administrador.local>
2023-02-07 16:48:56 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,4 +6,7 @@ module.exports = {
     siteUrl: `https://www.yourdomain.tld`,
   },
   plugins: [],
+  flags: {
+    DEV_SSR: true,
+  }
 }
---
+++ src/components/AsyncComponent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AsyncComponent = () => {
+  return (
+    <>
+      <h1>Hello, I'm async!</h1>
+    </>
+  );
+};
+
+export default AsyncComponent;
